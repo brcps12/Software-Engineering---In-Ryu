952e859f5496b7fa05f596ba1992eb2b7f848628
DESKTOP-K465JFJ\Brcps <brcps12@gmail.com>
2016-11-21 15:48:03 +0900

    초기 Config 파일

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$dbsettings = Array(
+	"server"     => "localhost", 			// MySQL server name.
+	"user"       => "username",	 		// MySQL username.
+	"pass"       => "password", 			// MySQL password.
+	"name"       => "db", 			// MySQL database name.
+);
+?>
