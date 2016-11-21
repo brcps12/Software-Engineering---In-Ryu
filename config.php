456baf176c224b360febc2c0ffcf64c27cdadbb7
brcps12 <brcps12@gmail.com>
2016-11-21 15:53:11 +0900

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbsettings = Array(
-	"server"     => "localhost", 			// MySQL server name.
-	"user"       => "username",	 		// MySQL username.
-	"pass"       => "password", 			// MySQL password.
-	"name"       => "db", 			// MySQL database name.
-);
-?>
