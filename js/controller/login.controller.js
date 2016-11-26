7128522dde58beed51d04061c1efa873769ae6f0
DESKTOP-K465JFJ\Brcps <brcps12@gmail.com>
2016-11-26 20:59:03 +0900

    로그인 페이지 추가 - 스타일 수정 - 로그인 적용

--- js/controller/login.controller.js
+++ js/controller/login.controller.js
@@ -6,12 +6,37 @@
 		.module(APP_NAME)
 		.controller('loginController', loginController);
 
-	loginController.$inject = ['$rootScope', '$http', 'AppConfig', '$stateParams'];
+	loginController.$inject = ['$rootScope', '$http', 'AppConfig', '$stateParams', 'loginService'];
 
-	function loginController($rootScope, $http, AppConfig, $stateParams) {
-		var rs = this;
+	function loginController($rootScope, $http, AppConfig, $stateParams, loginService) {
+		let rs = this;
 
-		rs.loginstatus=false;
+		rs.loginSubmit = loginSubmit;
+
+		function loginSubmit() {
+			return $http.post('/api/login', {
+				username: rs.username,
+				password: rs.password
+			}).success(function(r) {
+				if(r.request.result == 'success') {
+					loginService.addUser(r.request.user);
+
+					$rootScope.$broadcast('loginSuccess');
+				} else {
+					swal(
+						'Error',
+						r.request.msg,
+						'error'
+					)
+				}
+			}).error(function(r) {
+				swal(
+					'Error',
+					'서버 에러가 발생하였습니다',
+					'error'
+				)
+			})
+		}
 	}
 
 })(__APP_NAME__);
