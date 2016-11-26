32d62f5d31a6653f46d7d39a100fcd0e751270a5
DESKTOP-K465JFJ\Brcps <brcps12@gmail.com>
2016-11-26 19:16:56 +0900

    no message

--- js/controller/register.controller.js
+++ js/controller/register.controller.js
@@ -10,6 +10,16 @@
 
 	function registerController($rootScope, $http, AppConfig, $stateParams) {
 		var rs = this;
+
+		rs.message = [
+			'1번고객',
+			'2번고객',
+			'3번고객',
+			'4번고객',
+		];
+
+		rs.test = false;
+		rs.test2 = true;
 	}
 
 })(__APP_NAME__);
